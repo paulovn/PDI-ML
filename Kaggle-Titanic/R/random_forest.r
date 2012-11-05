a0a7793bded5358ce8067a63dfcbd76074eeb93e
José Gregorio Escalada Sardina <jges@tid.es>
2012-11-05 10:25:29 +0100

    Fix errors
    Signed-off-by: José Gregorio Escalada Sardina <jges@tid.es>

--- Kaggle-Titanic/R/random_forest.r
+++ Kaggle-Titanic/R/random_forest.r
@@ -4,6 +4,7 @@ library(randomForest)
 find_wife <- function(name, allnames)
 {
     transf.name <- sub("Mr.", "Mrs.", name, fixed=TRUE)
+    #print("possible wife:");
     #print(transf.name)
     return(grep(transf.name, allnames))
 }
@@ -11,19 +12,26 @@ find_wife <- function(name, allnames)
 find_husband <- function(name, allnames) {
     transf.name <- sub("Mrs.", "Mr.", name, fixed=TRUE)
     transf.name <- sub(" \\(..*\\)", "", transf.name, fixed=FALSE)
+    #print("possible husband:");
     #print(transf.name)
     return(grep(transf.name, allnames))
 }
 
 find_spouse_age <- function(person, allpersons, allnames) {
     transf.name <- sub(" \"..*\"", "", person$name, fixed=FALSE)
-print(transf.name)
+    #print("person:")
+    #print(transf.name)
 
     if (person$sex == "female") {
-        if ((person$sibsp > 0) & (length(grep("Mrs.", transf.name)) > 0) {
+        if ((person$sibsp > 0) & (length(grep("Mrs.", transf.name)) > 0)) {
             husband <- find_husband(transf.name, allnames)
+            #print("husband found")
+            #print(husband)
             if (length(husband) > 0) {
-                return(allpersons[husband]$age - 7)
+                spouse.age <- allpersons[husband,]$age;
+                if (is.na(spouse.age) == FALSE) {
+                  return(spouse.age - 7)
+                }
             }
             return (0)
         }
@@ -31,8 +39,13 @@ print(transf.name)
     } else {
         if (person$sibsp > 0) {
             wife <- find_wife(transf.name, allnames)
+            #print("wife found")
+            #print(wife)
             if (length(wife) > 0) {
-                return(allpersons[wife]$age + 7)
+                spouse.age <- allpersons[wife,]$age;
+                if (is.na(spouse.age) == FALSE) {
+                return(spouse.age + 7)
+                }
             }
             return (0)
         }
@@ -68,58 +81,76 @@ test  <- data.frame( age=test_all$age,
                      pclass=test_all$pclass,
                      sex=as.integer(factor(test_all$sex)) )
 
-#Useless
-#names <- train_all$name
-#married = c(1:length(train_all$name))
-#married[] <- 0
-#married[grep("Mrs.", names)] <- 1
+#Just a quick test for married
+names <- train_all$name
+married = c(1:length(train_all$name))
+married[] <- 0
+married[grep("Mrs.", names)] <- 1
 
 # "female" -> 1, "male" -> 2
 
 #Completing missing age
 #First trying to get the spouse age
 age<-train_all$age
+sum(is.na(train_all$age)) 
 for (i in 1:ntrain) {
     if (is.na(age[i])) {
-        print(train_all[i,])
-        new.age <- find_spouse_age(train_all[i,], allpersons, allnames)
-        if (new.age) {
+        #print("age NA")
+        #print(i)
+        #print(train_all[i,])
+        new.age <- find_spouse_age(train_all[i,], allsets, allnames)
+        if (new.age[1] != 0) {
+            #print("new age:")
             #print(new.age)
             age[i] <- new.age
         }
     }
 }
+sum(is.na(age)) 
 #Then, some general heuristics
 age[is.na(age) & (train_all$sex == "female") & (married == 1)] <- 40
+sum(is.na(age))
 age[is.na(age) & (train_all$sex == "female") & (train_all$parch == 0)] <- 25
+sum(is.na(age))
 age[is.na(age) & (train_all$sex == "female")] <- 10
+sum(is.na(age))
 age[is.na(age) & ((train_all$sibsp > 1) | (train_all$parch > 0))] <- 10
+sum(is.na(age))
 age[is.na(age) & (train_all$sibsp == 1)] <- 50
+sum(is.na(age))
 age[is.na(age) & (train_all$sibsp == 0)] <- 30
+sum(is.na(age))
 train$age<-age
 
-#Also useless for test
-#names <- test_all$name
-#married = c(1:length(test_all$name))
-#married[] <- 0
-#married[grep("Mrs.", names)] <- 1
+#Just a quick test for married, for test
+names <- test_all$name
+married = c(1:length(test_all$name))
+married[] <- 0
+married[grep("Mrs.", names)] <- 1
 
 #Same age estimation for test dataset
 age<-test_all$age
 for (i in 1:ntest) {
     if (is.na(age[i])) {
-        new.age <- find_spouse_age(test_all[i,], allpersons, allnames)
-        if (new.age) {
+        new.age <- find_spouse_age(test_all[i,], allsets, allnames)
+        if ((is.na(new.age) == FALSE) && (new.age[1] > 0)) {
             age[i] <- new.age
         }
     }
 }
+sum(is.na(age))
 age[is.na(age) & (test_all$sex == "female") & (married == 1)] <- 40
+sum(is.na(age))
 age[is.na(age) & (test_all$sex == "female") & (test_all$parch == 0)] <- 25
+sum(is.na(age))
 age[is.na(age) & (test_all$sex == "female")] <- 10
+sum(is.na(age))
 age[is.na(age) & ((test_all$sibsp > 1) | (test_all$parch > 0))] <- 10
+sum(is.na(age))
 age[is.na(age) & (test_all$sibsp == 1)] <- 50
+sum(is.na(age))
 age[is.na(age) & (test_all$sibsp == 0)] <- 30
+sum(is.na(age))
 test$age<-age
 
 #There are fare missing values in test. Anyway, we set both datasets
